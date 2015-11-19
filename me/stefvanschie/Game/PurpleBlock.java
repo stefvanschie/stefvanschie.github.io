e0768053c0a4faf60a7b56b533d750acb81e1ec5
stefvanschie <stef@van-schie.eu>
2015-11-19 21:03:32 +0100

    Create PurpleBlock.java
    Deprecated

---
+++ me/stefvanschie/Game/PurpleBlock.java
@@ -0,0 +1,29 @@
+package me.stefvanschie.Game;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class PurpleBlock extends GameObject {
+
+	public PurpleBlock(int x, int y, ID id) {
+		super(x, y, id);
+		setX(0);
+		setY(0);
+	}
+
+	@Override
+	public void tick() {
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		
+	}
+
+	@Override
+	public Rectangle getBounds() {
+		return null;
+	}
+
+}

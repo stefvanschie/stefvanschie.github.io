810bd6537a3483596f5e7c7cbf9506cbedfd365b
stefvanschie <stef@van-schie.eu>
2015-11-19 21:01:10 +0100

    Create Beam.java

---
+++ me/stefvanschie/Game/Beam.java
@@ -0,0 +1,51 @@
+package me.stefvanschie.Game;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Beam extends GameObject {
+
+	Handler handler;
+	
+	
+	public Beam (int x, int y, ID id, Handler handler) {
+		super(x, y, id);
+		this.handler = handler;
+		setX(165);
+		setY(450);
+	}
+
+	public void tick() {
+		x += velX;
+		x = Game.clamp(x, 0, Game.width - 155);
+		handler.addObject(new Trail(x, y, ID.Trail, Color.WHITE, 150, 25, 0.02f, handler));
+		collision();
+	}
+
+	public void render(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(x, y, 150, 25);
+	}
+
+	private void collision() {
+		for (int i = 0; i < handler.object.size(); i++) {
+			GameObject tempObject = handler.object.get(i);
+			if (tempObject.getID() == ID.Ball) {
+				if (getBounds().intersects(tempObject.getBounds())) {
+					tempObject.velY *= -1;
+					if (velX > 0) {
+						tempObject.velX = 3;
+					} else if (velX < 0) {
+						tempObject.velX = -3;
+					}
+				}
+			}
+		}
+	}
+	
+	public Rectangle getBounds() {
+		return new Rectangle(x, y, 150, 25);
+	}
+	
+}

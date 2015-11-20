455ad72ac478f0ba9ae040fd3409e252b6fa08e5
stefvanschie <stef@van-schie.eu>
2015-11-20 12:49:44 +0100

    Update Menu.java

--- me/stefvanschie/Game/Menu.java
+++ me/stefvanschie/Game/Menu.java
@@ -11,12 +11,10 @@ import me.stefvanschie.Game.Game.STATE;
 public class Menu extends MouseAdapter {
 
 	private Game game;
-	private Handler handler;
 	private Health health;
 	
-	public Menu(Game game, Handler handler, Health health) {
+	public Menu(Game game, Health health) {
 		this.game = game;
-		this.handler = handler;
 		this.health = health;
 	}
 	
@@ -26,18 +24,18 @@ public class Menu extends MouseAdapter {
 		
 		if (mouseOver(mx, my, 16, 460, 150, 50) && game.gameState == STATE.Menu) {
 			game.gameState = STATE.Game;
-			handler.addObject(new Ball(100, 100, ID.Ball, handler, health));
-			handler.addObject(new Beam(100, 100, ID.Beam, handler));
+			Handler.addObject(new Ball(100, 100, ID.Ball, health));
+			Handler.addObject(new Beam(100, 100, ID.Beam));
 			for (int i = 0; i < 4; i++) {
 				for (int j = 0; j < 4; j ++) {
-					handler.addObject(new BlueBlock(48 + j * 96, 64 + i * 32, ID.BlueBlock, handler, game));
+					Handler.addObject(new BlueBlock(48 + j * 96, 64 + i * 32, ID.BlueBlock, game));
 				}
 			}
 		}
 		
 		if (game.gameState == STATE.Game) {
-			for (int i = 0; i < handler.object.size(); i++) {
-				GameObject tempObject = handler.object.get(i);
+			for (int i = 0; i < Handler.objects.size(); i++) {
+				GameObject tempObject = Handler.object.get(i);
 				if (tempObject.getID() == ID.Ball && tempObject.getVelX() == 0 && tempObject.getVelY() == 0) {
 					tempObject.setVelY(3);
 					tempObject.setVelX(3);
@@ -46,20 +44,12 @@ public class Menu extends MouseAdapter {
 		}
 	}
 	
-	public void mouseReleased(MouseEvent e) {
-		
-	}
-	
 	private boolean mouseOver(int mx, int my, int x, int y, int width, int height) {
 		if (mx > x && mx < x + width) {
 			if (my > y && my < y + height)
 				return true;
 		}
 		return false;
-	}
-	
-	public void tick() {
-		
 	}
 	
 	public void render(Graphics g) {

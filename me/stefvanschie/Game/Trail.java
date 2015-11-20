4003291ad69e14ed902ae2865f33a4bd33eeba4d
stefvanschie <stef@van-schie.eu>
2015-11-20 12:52:45 +0100

    Update Trail.java

--- me/stefvanschie/Game/Trail.java
+++ me/stefvanschie/Game/Trail.java
@@ -9,16 +9,14 @@ import java.awt.Rectangle;
 public class Trail extends GameObject {
 
 	private float alpha = 1;
-	private Handler handler;
 	private Color color;
 	private int width, height;
 	private float life;
 	
 	//life = 0.001 - 0.1
 	
-	public Trail(int x, int y, ID id, Color color, int width, int height, float life, Handler handler) {
+	public Trail(int x, int y, ID id, Color color, int width, int height, float life) {
 		super(x, y, id);
-		this.handler = handler;
 		this.color = color;
 		this.width = width;
 		this.height = height;
@@ -29,7 +27,7 @@ public class Trail extends GameObject {
 		if (alpha > life) {
 			alpha-= life - 0.001f;
 		} else {
-			handler.removeObject(this);
+			Handler.removeObject(this);
 		}
 	}
 
@@ -47,7 +45,7 @@ public class Trail extends GameObject {
 	}
 	
 	public Rectangle getBounds() {
-		return null;
+		return new Rectangle(x, y, widht, length);
 	}
 
 }

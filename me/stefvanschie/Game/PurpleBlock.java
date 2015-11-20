fbe1094447617d1f56ca0ba8f8fb3feeb23340a0
stefvanschie <stef@van-schie.eu>
2015-11-20 12:34:41 +0100

    Update PurpleBlock.java

--- me/stefvanschie/Game/PurpleBlock.java
+++ me/stefvanschie/Game/PurpleBlock.java
@@ -7,23 +7,20 @@ public class PurpleBlock extends GameObject {
 
 	public PurpleBlock(int x, int y, ID id) {
 		super(x, y, id);
-		setX(0);
-		setY(0);
 	}
 
 	@Override
-	public void tick() {
-		
-	}
-
+	public void tick() {}
+	
 	@Override
 	public void render(Graphics g) {
-		
+		g.setColor(Color.PURPLE);
+		g.fillRect(x, y, 75, 25);
 	}
 
 	@Override
 	public Rectangle getBounds() {
-		return null;
+		return new Rectangle(x, y, 75, 25);
 	}
 
 }

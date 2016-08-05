b85097ad5e4b166c9465d92b8d202d5e3eedd347
stefvanschie <stefvanschiedev@gmail.com>
2016-08-05 20:48:21 +0200

    Create licensechooser.js

---
+++ projects/licensechooser.js
@@ -0,0 +1,103 @@
+var licenses = {
+  "GNU AGPLv3":{
+    "commercial":true,
+    "distribution":true,
+    "modification":true,
+    "patent":true,
+    "private":true,
+
+    "disclose":true,
+    "license-copyright":true,
+    "network":true,
+    "license":true,
+    "changes":true,
+
+    "liable":true,
+    "trademark":false
+  },
+  "GNU GPLv3":{
+    "commercial":true,
+    "distribution":true,
+    "modification":true,
+    "patent":true,
+    "private":true,
+
+    "disclose":true,
+    "license-copyright":true,
+    "license":true,
+    "changes":true,
+
+    "liable":true,
+    "trademark":false
+  },
+  "GNU LGPLv3":{
+    "commercial":true,
+    "distribution":true,
+    "modification":true,
+    "patent":true,
+    "private":true,
+
+    "disclose":true,
+    "license-copyright":true,
+    "license":true,
+    "changes":true,
+
+    "liable":true,
+    "trademark":false
+  },
+  "Mozilla Public License 2.0":{
+    "commercial":true,
+    "distribution":true,
+    "modification":true,
+    "patent":true,
+    "private":true,
+
+    "disclose":true,
+    "license-copyright":true,
+    "license":true,
+
+    "liable":true,
+    "trademark":true
+  },
+  "Apache License 2.0":{
+    "commercial":true,
+    "distribution":true,
+    "modification":true,
+    "patent":true,
+    "private":true,
+
+    "license-copyright":true,
+    "changes":true,
+
+    "liable":true,
+    "trademark":true
+  },
+  "MIT License":{
+    "commercial":true,
+    "distribution":true,
+    "modification":true,
+    "patent":false,
+    "private":true,
+
+    "license-copyright":true,
+
+    "liable":true,
+    "trademark":false
+  },
+  "The Unlicense":{
+    "commercial":true,
+    "distribution":true,
+    "modification":true,
+    "patent":false,
+    "private":true,
+
+    "liable":true,
+    "trademark":false
+  }
+};
+
+$(document).ready(function() {
+  $("#btn-license").click(function() {
+    
+  });
+});

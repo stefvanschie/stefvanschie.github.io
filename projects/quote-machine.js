b282acabdc187ee8d75f03621271268dccc2c831
stefvanschie <stefvanschiedev@gmail.com>
2016-11-12 13:32:01 +0100

    Update quote-machine.js

--- projects/quote-machine.js
+++ projects/quote-machine.js
@@ -2,9 +2,11 @@ var quotes = [
   "You can do anything, but not everything.",
   "Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.",
   "The richest man is not he who has the most, but he who needs the least."
-]
+];
 
 $(document).ready(function() {
+  alert("test");
+  
   $("#quote").text(quotes[Math.floor(Math.random() * quotes.length)]);
   
   $("button").click(function() {

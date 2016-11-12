59c42fba110df77d9681de234ca890114c761eb7
stefvanschie <stefvanschiedev@gmail.com>
2016-11-12 13:16:05 +0100

    Update quote-machine.js

--- projects/quote-machine.js
+++ projects/quote-machine.js
@@ -1,1 +1,9 @@
+var qutoes = [
+  "You can do anything, but not everything.",
+  "Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.",
+  "The richest man is not he who has the most, but he who needs the least."
+]
 
+$(document).ready(function() {
+
+});

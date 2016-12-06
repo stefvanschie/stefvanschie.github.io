081f14300990c500dcf0f4541aeacd282e974168
stefvanschie <stefvanschiedev@gmail.com>
2016-12-06 15:41:50 +0100

    Update quote-machine.js

--- projects/quote-machine.js
+++ projects/quote-machine.js
@@ -1,6 +1,6 @@
 var quotes = [
-  "You can do anything, but not everything.</ br><i> - David Allen</i>",
-  "Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.</ br><i> - Antoine de Saint-Exupéry</i>",
+  "You can do anything, but not everything.</ br></ br><i> - David Allen</i>",
+  "Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.</ br></ br><i> - Antoine de Saint-Exupéry</i>",
   "The richest man is not he who has the most, but he who needs the least."
 ];
 

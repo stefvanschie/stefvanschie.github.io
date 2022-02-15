4e8d203f70cef8672701eb87c80e56ae652e12c9
stefvanschie <stefvanschiedev@gmail.com>
2022-02-15 20:07:42 +0100

    Add 0.2.1 javadocs

---
+++ if/javadocs/0.2.1/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.stefvanschie.inventoryframework"},{"l":"com.github.stefvanschie.inventoryframework.pane"},{"l":"com.github.stefvanschie.inventoryframework.pane.util"},{"l":"com.github.stefvanschie.inventoryframework.util"}];updateSearchResults();
